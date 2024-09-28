7bf1c652964c96b7330484ffbd6e3265482284e8
fatimah rana <fatimahrana2908@gmail.com>
2024-09-28 00:18:24 -0400

    commit

--- src/education.jsx
+++ src/education.jsx
@@ -1,9 +1,25 @@
 /* | education.jsx | FATIMAH MOHAMMED RANA | 301342519 | SEPT 27 2024 */
+import React from 'react';
+
 export default function Education() {
     return (
         <>
-            <p>Education.Qualification</p>
+            <h1 id="services">My Services</h1>
+            <ul>
+                <li>Web Development</li>
+                <img src="/public/HDcyborg.JPG" alt="Web Development" className="bordered-image" />
+                
+                <li>C# Programming</li>
+                <img src="cyber.JPG" alt="C# Programming" className="bordered-image" />
+                
+                <li>Java Programming</li>
+                <img src="/public/java.jpg" alt="Java Programming" className="bordered-image" />
+                
+                <li>Music Production</li>
+                <li>DJing</li>
+                <li>Sound Processing</li>
+                <img src="/public/music.jpg" alt="Music Production" className="bordered-image" />
+            </ul>
         </>
     );
 }
-
